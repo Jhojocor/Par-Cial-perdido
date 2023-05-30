ac300b86ef6cdf84737451af98bbcffe1ae81d27
Jhohanson Jovel Cordoba <1082214060@u.icesi.edu.co>
2023-05-30 12:58:44 -0500

    Primer commit de algo

---
+++ src/store/action.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0JXWBBtvazEuBWTHVv4lacbEG1UOZsLk",
+  authDomain: "parcialtry.firebaseapp.com",
+  projectId: "parcialtry",
+  storageBucket: "parcialtry.appspot.com",
+  messagingSenderId: "830286527310",
+  appId: "1:830286527310:web:dab282f950928fe74fbc1f"
+};
+
+const app = initializeApp(firebaseConfig);
